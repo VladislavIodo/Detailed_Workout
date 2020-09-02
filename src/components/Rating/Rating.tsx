e738ba1a220f692d755de3e9f962a458b71b2007
IodoV <vladislav_iodo@mail.ru>
2020-09-02 15:17:47 +0300

    2_Workout

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type RatingPropsType = {
+    value: 0 | 1 | 2 | 3 | 4 | 5
+}
+
+export function Rating(props: RatingPropsType) {
+    return (
+        <div>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+        </div>
+    )
+}
+
+type StarPropsType = {
+    selected: boolean
+}
+
+function Star(props: StarPropsType) {
+    if (props.selected === true) {
+        return <span><b>Star</b></span>
+    } else {
+        return <span>Star</span>
+    }
+}
