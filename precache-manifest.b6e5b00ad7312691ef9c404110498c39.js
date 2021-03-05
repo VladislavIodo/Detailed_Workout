c521eeea3b5cdb6021ed4fc91088a2373eda82cd
VladislavIodo <vladislav_iodo@mail.ru>
2021-03-05 23:59:26 +0300

    Updates

---
+++ precache-manifest.b6e5b00ad7312691ef9c404110498c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cf8beeb469ddd6841d004aeb867b40",
+    "url": "/Detailed_Workout/index.html"
+  },
+  {
+    "revision": "a198801596c33afb0cd0",
+    "url": "/Detailed_Workout/static/css/main.6c69b40a.chunk.css"
+  },
+  {
+    "revision": "6afa0b0eac2c670316a4",
+    "url": "/Detailed_Workout/static/js/2.e3fe2741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Detailed_Workout/static/js/2.e3fe2741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a198801596c33afb0cd0",
+    "url": "/Detailed_Workout/static/js/main.9d4f4586.chunk.js"
+  },
+  {
+    "revision": "cc5d0f873fbec563e783",
+    "url": "/Detailed_Workout/static/js/runtime-main.bcf37129.js"
+  }
+]);
